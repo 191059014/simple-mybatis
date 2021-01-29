ff774dde9932960f8d7b62f7524c87afb0ab5097
黄彪 <191059014@qq.com>
2021-01-29 19:35:30 +0800

    优化代码

--- src/main/java/com/hb/mybatis/helper/Delete.java
+++ src/main/java/com/hb/mybatis/helper/Delete.java
@@ -9,14 +9,21 @@ package com.hb.mybatis.helper;
 public class Delete {
 
     /**
+     * 删除sql语句模板
+     */
+    public static final String DELETE_SQL_TEMPLATE = "delete from %s %s";
+
+    /**
      * 构建删除sql语句
      *
-     * @param tableName 表名
-     * @param where     条件集合
+     * @param tableName
+     *            表名
+     * @param where
+     *            条件集合
      * @return 删除sql语句
      */
     public static String buildSql(String tableName, Where where) {
-        return "delete from " + tableName + where.getWhereSql();
+        return String.format(DELETE_SQL_TEMPLATE, tableName, where.getWhereSql());
     }
 
 }
